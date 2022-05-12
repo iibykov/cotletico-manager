498bb50737d4ad95802ec3480217f1dcf9c95b93
Dmitriy Belyaev <d.belyaev1988@yandex.ru>
2022-05-12 23:08:09 +0300

    add method ideal_squad

--- src/cotletico_manager.py
+++ src/cotletico_manager.py
@@ -87,3 +87,42 @@ def teams_from_json(filename: str):
 
 teams = teams_from_json('../data/players.json')
 print(len(teams))
+
+
+def ideal_squad(team, formation='4-4-2'):
+    first_11 = []
+    goalkeepers, defenders, midfielders, forwards = [], [], [], []
+    def_positions = ('LB', 'CB', 'RB', 'LWB', 'RWB')
+    mid_positions = ('CDM', 'LM', 'CM', 'RM', 'CAM')
+    frw_positions = ('LW', 'RW', 'ST', 'SS')
+
+    if not isinstance(formation, str) or len(formation.split('-')) != 3 or \
+            not all([i.isdigit() for i in (formation.split('-'))]) or sum(map(int, formation.split('-'))) != 10:
+        formation = '4-4-2'
+    def_slots, mid_slots, frw_slots = map(int, formation.split('-'))
+
+    for player in team.players:
+        if player.position == 'GK':
+            goalkeepers.append(player)
+        elif player.position in def_positions:
+            defenders.append(player)
+        elif player.position in mid_positions:
+            midfielders.append(player)
+        elif player.position in frw_positions:
+            forwards.append(player)
+
+    if def_slots > len(defenders) or mid_slots > len(midfielders) or frw_slots > len(forwards):
+        def_slots, mid_slots, frw_slots = 4, 4, 2
+
+    first_11.append(max(goalkeepers))
+    defenders.sort(reverse=True)
+    for i in range(def_slots):
+        first_11.append(defenders[i])
+    midfielders.sort(reverse=True)
+    for i in range(mid_slots):
+        first_11.append(midfielders[i])
+    forwards.sort(reverse=True)
+    for i in range(frw_slots):
+        first_11.append(forwards[i])
+
+    return first_11
