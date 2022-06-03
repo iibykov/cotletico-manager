286c533bcc837879a6d2af2b4183f6735fb5b612
i00535886 <bykov.i.i.1987@gmail.com>
2022-06-04 00:50:17 +0300

    Removed the time field from the event class

--- src/event.py
+++ src/event.py
@@ -5,7 +5,6 @@ import json
 class Event:
     def __init__(self, messages):
         self.messages = messages
-        self.time = time
 
     def print_message(self, **params):
         message = "".join(sample(self.messages, 1))
