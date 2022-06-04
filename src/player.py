830a03a31c5ca234554d17009e683d67e6f44341
i00535886 <bykov.i.i.1987@gmail.com>
2022-06-04 19:07:18 +0300

    Refactoring the Player class

--- src/player.py
+++ src/player.py
@@ -1,5 +1,14 @@
+from enum import Enum
+
+
+class PlayerStatus(Enum):
+    OK = 0
+    INJURED = 1
+    RED_CARD = 2
+
+
 class Player:
-    def __init__(self, name, surname, position, rating, birthdate, number, team, statistics):
+    def __init__(self, name, surname, position, rating, birthdate, number, team, status=PlayerStatus.OK, statistics=[]):
         self.name = name
         self.surname = surname
         self.position = position
@@ -7,7 +16,12 @@ class Player:
         self.birthdate = birthdate
         self.number = number
         self.team = team
+        self.status = status
         self.statistics = statistics
+
+    def __str__(self):
+        return f'Player({self.name}, {self.surname}, {self.position}, {self.rating}, {self.birthdate}, {self.number},' \
+               f'{self.team}, {self.status}, {self.statistics})'
 
     def update_statistics(self, statistics):
         self.statistics = statistics
@@ -15,5 +29,5 @@ class Player:
     def __lt__(self, other):
         return self.rating < other.rating
 
-    def __add__(self, other):
-        return self.rating + self.rating
+    def can_play(self):
+        return self.status == PlayerStatus.OK
